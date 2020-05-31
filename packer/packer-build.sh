e18443fa7a95fbad28175efc0846b1e4a4d2a992
Garvin <Garvin@local>
2020-05-31 10:00:57 -0400

    fix azcopy command

--- packer/packer-build.sh
+++ packer/packer-build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 #Bake image
-echo "Building image with packer"
+#Building image with packer
 cd /root/config
 packer fix template.json > template-fixed.json
 PACKER_LOG=1 packer build template-fixed.json
 
-echo "Packer image build complete. Converting to VHD format."
-
+#Packer image build complete. Converting to VHD format.
 qemu-img convert -f raw -o subformat=fixed,force_size -O vpc disk/baked baked.vhd
--- packer/upload-vhd.sh
+++ packer/upload-vhd.sh
@@ -5,8 +5,8 @@ SAS_TOKEN=$2
 
 cd /root/config
 
-#--quiet: overwrite existing files
+#--overwrite on by default: overwrite existing files
 azcopy cp \
     baked.vhd \
     "$IMAGE_URL?$SAS_TOKEN" \
-    --blob-type page 
+    --blob-type PageBlob 
